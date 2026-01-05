3ced6067c509d63405765f723e51ee542bf99172
Nikhita <nikhita.qa@gmail.com>
2026-01-05 12:55:27 +0530

    Practice

---
+++ src/test/java/com/testing/framework/practice/test01_java/Test24_CalculateStringSum.java
@@ -0,0 +1,30 @@
+package com.testing.framework.practice.test01_java;
+
+public class Test24_CalculateStringSum {
+
+    //Caluclate the sum of the digits from the given string
+    //12+34+56+10 = 112
+    //https://www.youtube.com/watch?v=y3yVNmQ-qkQ
+
+    public static void main(String[] args) {
+        String str = "12abc34xyz56lmn10";
+
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+        for (char ch : str.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                sb.append(ch);
+            } else {
+                if (sb.length() > 0) {
+                    sum += Integer.parseInt(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+        }
+        //This if is only needed when you have digits at the last
+        if (sb.length() > 0) {
+            sum += Integer.parseInt(sb.toString());
+        }
+        System.out.println(sum);
+    }
+}
